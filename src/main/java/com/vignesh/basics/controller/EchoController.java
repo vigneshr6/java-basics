834b43c7b1135bb38341b56bdd2f9a9959782823
vignesh <iamvigneshr6@gmail.com>
2024-08-21 20:53:39 +0530

    upgraded to java21

--- src/main/java/com/vignesh/basics/controller/EchoController.java
+++ src/main/java/com/vignesh/basics/controller/EchoController.java
@@ -1,11 +1,11 @@
 package com.vignesh.basics.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
--- src/main/java/com/vignesh/basics/controller/ExceptionHandler.java
+++ src/main/java/com/vignesh/basics/controller/ExceptionHandler.java
@@ -2,6 +2,7 @@ package com.vignesh.basics.controller;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         return new ResponseEntity<>("Given input is not a valid string", HttpStatus.BAD_REQUEST);
     }
 }
